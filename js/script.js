4adf19f1254fd7e8706df576a6a2d206eec8bfef
DaniloDuarte <danilojoseduarte@gmail.com>
2023-02-01 01:00:51 -0300

    Arquivos JavaScript editados

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-const citySearchBar = document.getElementById('header-city-search-bar')
-const citySearchButton = document.getElementById('city-search-button')
 
-const currentDate = document.getElementById('current-date')
-const cityName = document.getElementById('city-name')
-const weatherIcon = document.getElementById('weather-icon')
-const weatherDescritption = document.getElementById('weather-description')
-const currentTemperature = document.getElementById('current-temperature')
-const windSpeed = document.getElementById('wind-speed')
-const feelsLikeTemperature = document.getElementById('feels-like-temeprature')
-const currentHumidity = document.getElementById('current-humidity')
-const sunriseTime = document.getElementById('sunrise-time')
-const sunsetTime = document.getElementById('sunset-time')
+//Interação
+const citySearchBar = document.getElementById('header-city-search-bar');
+const citySearchButton = document.getElementById('city-search-button');
+
+//Exibição
+const currentDate = document.getElementById('current-date');
+const cityName = document.getElementById('city-name');
+const weatherIcon = document.getElementById('weather-icon');
+const weatherDescritption = document.getElementById('weather-description');
+const currentTemperature = document.getElementById('current-temperature');
+const windSpeed = document.getElementById('wind-speed');
+const feelsLikeTemperature = document.getElementById('feels-like-temeprature');
+const currentHumidity = document.getElementById('current-humidity');
+const sunriseTime = document.getElementById('sunrise-time');
+const sunsetTime = document.getElementById('sunset-time');
