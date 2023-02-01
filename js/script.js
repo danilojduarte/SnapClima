51f4b3379474e5206e2e7bd2f10cbb838a1cb24e
DaniloDuarte <danilojoseduarte@gmail.com>
2023-02-01 00:58:01 -0300

    Arquivos gerados JavaScript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-function somar (num1, num2) {
-   alert(num1 + num2)
-}
+const citySearchBar = document.getElementById('header-city-search-bar')
+const citySearchButton = document.getElementById('city-search-button')
 
-somar(10, 2)
+const currentDate = document.getElementById('current-date')
+const cityName = document.getElementById('city-name')
+const weatherIcon = document.getElementById('weather-icon')
+const weatherDescritption = document.getElementById('weather-description')
+const currentTemperature = document.getElementById('current-temperature')
+const windSpeed = document.getElementById('wind-speed')
+const feelsLikeTemperature = document.getElementById('feels-like-temeprature')
+const currentHumidity = document.getElementById('current-humidity')
+const sunriseTime = document.getElementById('sunrise-time')
+const sunsetTime = document.getElementById('sunset-time')
